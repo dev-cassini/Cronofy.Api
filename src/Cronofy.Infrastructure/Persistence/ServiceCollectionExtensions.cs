df117269bf46366e38176e303adc668b5797a853
Ryan Dremza <ryan.dremza@gmail.com>
2023-06-11 21:35:53 +0100

    Register read db context

--- src/Cronofy.Infrastructure/Persistence/ServiceCollectionExtensions.cs
+++ src/Cronofy.Infrastructure/Persistence/ServiceCollectionExtensions.cs
@@ -1,4 +1,5 @@
 using Cronofy.Infrastructure.Persistence.Databases.Postgres;
+using Cronofy.Infrastructure.Persistence.Read;
 using Cronofy.Infrastructure.Persistence.Write;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -9,6 +10,7 @@ internal static class ServiceCollectionExtensions
 {
     internal static void AddPersistence(this IServiceCollection serviceCollection, IConfiguration configuration)
     {
+        serviceCollection.AddPostgresDatabase<CronofyReadDbContext>(configuration);
         serviceCollection.AddPostgresDatabase<CronofyWriteDbContext>(configuration);
     }
 }
